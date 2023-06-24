c925223bd30c744bf6d70e6a916162b720cb5e89
yunqiqiliang <132561395+yunqiqiliang@users.noreply.github.com>
2023-06-24 16:46:18 +0800

    Update 05_🔹_Realtime Data Transfer.py

--- pages/05_🔹_Realtime Data Transfer.py
+++ pages/05_🔹_Realtime Data Transfer.py
@@ -26,6 +26,7 @@ def main():
     gui.space(1)
     st.subheader("Real time data transfer")
     # Get data
+    query = sql.DATA_TRANSFER_QUERY
     df = sf.sql_to_dataframe(
         query.format(date_from=date_from, date_to=date_to)
     )
