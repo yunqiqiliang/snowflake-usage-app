064e93af34457073b49d058bfcf330bce2cd5b59
yunqiqiliang <132561395+yunqiqiliang@users.noreply.github.com>
2023-06-24 17:17:14 +0800

    Update 05_🔹_Realtime Data Transfer.py

--- pages/05_🔹_Realtime Data Transfer.py
+++ pages/05_🔹_Realtime Data Transfer.py
@@ -1,6 +1,6 @@
 import plost
 import streamlit as st
-from streamlit_autorefresh import st_autorefresh
+# from streamlit_autorefresh import st_autorefresh
 
 st.set_page_config(
     page_title="Usage Insights app - Real time data transfer", page_icon="🔹"
