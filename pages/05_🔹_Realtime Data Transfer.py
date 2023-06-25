8720ff48187783d6e092b830c55370e52782b92a
yunqiqiliang <132561395+yunqiqiliang@users.noreply.github.com>
2023-06-25 22:15:44 +0800

    Update 05_🔹_Realtime Data Transfer.py

--- pages/05_🔹_Realtime Data Transfer.py
+++ pages/05_🔹_Realtime Data Transfer.py
@@ -5,24 +5,6 @@ from utils import charts, gui, processing
 from utils import snowflake_connector as sf
 from utils import sql as sql
 
-from streamlit.server.util import get_report_ctx
-
-class SessionState:
-    def __init__(self, **kwargs):
-        self.__dict__.update(kwargs)
-
-def get_session(key):
-    session_state = SessionState()
-    session_id = get_report_ctx().session_id
-    
-    if hasattr(st, '_session_state'):
-        if key not in st._session_state:
-            st._session_state[key] = session_state
-        session_state = st._session_state[key]
-    else:
-        st._session_state = {key: session_state}
-
-    return session_state
 
 # st.set_page_config(
 #     page_title="Usage Insights app - Real time data transfer", page_icon="🔹", layout="wide"
@@ -42,11 +24,6 @@ def main():
 
     gui.space(1)
     st.subheader("Real time data transfer")
-    state = get_session('count')
-
-    if state.count is None:
-        state.count = 0
-
    
     while True:
         # Get data
@@ -58,12 +35,9 @@ def main():
         
         
         total_customer_count = df.iloc[0, 0]
-        if state.count == 0:
-            state.count = total_customer_count
-        new_customer_count = total_customer_count - state.count
         
-        st.metric(label="客户总数", value="{:,}".format(total_customer_count), delta="{:,}".format(new_customer_count))
-        state.count = total_customer_count
+        # st.metric(label="客户总数", value="{:,}".format(total_customer_count), delta="{:,}".format(new_customer_count))
+        st.metric(label="客户总数", value="{:,}".format(total_customer_count))
         # query = sql.CUSTOMERS_LIMIT_10
         # df = sf.sql_to_dataframe(
         #     query.format(date_from=date_from, date_to=date_to)
