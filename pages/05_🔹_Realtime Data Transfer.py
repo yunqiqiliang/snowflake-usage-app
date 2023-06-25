acc487ab82570af5c523ac5fb0d7f4387a16553f
yunqiqiliang <132561395+yunqiqiliang@users.noreply.github.com>
2023-06-25 10:37:34 +0800

    Update 05_🔹_Realtime Data Transfer.py

--- pages/05_🔹_Realtime Data Transfer.py
+++ pages/05_🔹_Realtime Data Transfer.py
@@ -32,11 +32,11 @@ def main():
         )
         st.table(df)
     
-        query = sql.CUSTOMERS_LIMIT_10
-        df = sf.sql_to_dataframe(
-            query.format(date_from=date_from, date_to=date_to)
-        )
-        st.table(df)
+        # query = sql.CUSTOMERS_LIMIT_10
+        # df = sf.sql_to_dataframe(
+        #     query.format(date_from=date_from, date_to=date_to)
+        # )
+        # st.table(df)
         # Wait for 2 seconds
         time.sleep(2)
     
