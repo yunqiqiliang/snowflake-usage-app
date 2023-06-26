e375306cdbbbd479bdead166d93838876b1e3054
yunqiqiliang <132561395+yunqiqiliang@users.noreply.github.com>
2023-06-26 20:28:46 +0800

    Update 05_🔹_Realtime Data Transfer.py

--- pages/05_🔹_Realtime Data Transfer.py
+++ pages/05_🔹_Realtime Data Transfer.py
@@ -47,7 +47,7 @@ def main():
         # )
         # st.table(df)
         # Wait for 10 seconds
-        time.sleep(10)
+        time.sleep(2)
     
         # Rerun the app to refresh the chart
         st.experimental_rerun()
